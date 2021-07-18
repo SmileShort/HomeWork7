a9c5fcfbaca4aeb3ea2c4e8e7279bbd7814a5579
SmileShort <86470906+SmileShort@users.noreply.github.com>
2021-07-18 15:36:31 +0500

    HomeWork7

---
+++ src/geekbrains/HomeWork7/Cat.java
@@ -0,0 +1,34 @@
+package geekbrains.HomeWork7;
+
+public class Cat {
+    private boolean isFull;
+    private int appetite;
+    private String name;
+
+    public Cat(int appetite, String name)
+    {
+        isFull = false;
+        this.appetite = appetite;
+        this.name = name;
+    }
+
+
+    public void eat(Plate plate)
+    {
+        if (plate.hasEnoughFoodFor(appetite) && !isFull)
+        {
+            System.out.println(name + " кушает из тарелки");
+            plate.decreaseFood(appetite);
+            isFull = true;
+        }
+        else if (!plate.hasEnoughFoodFor(appetite))
+        {
+            System.out.println(name + " недовольно рычит, ведь у тарелки видно дно!");
+        }
+        else if (isFull)
+        {
+            System.out.println(name + " уже сыт(а) и больше не хочет кушать!");
+        }
+    }
+
+}
---
+++ src/geekbrains/HomeWork7/Main.java
@@ -0,0 +1,27 @@
+package geekbrains.HomeWork7;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+            Cat[] cats = {
+                    new Cat(7, "Маня"),
+                    new Cat(8, "Мику"),
+                    new Cat(15, "Рыжик")
+            };
+
+            Plate plate = new Plate(20);
+
+            for (Cat cat : cats)
+            {
+                cat.eat(plate);
+            }
+
+            plate.increaseFood(30);
+
+            for (Cat cat : cats)
+            {
+                cat.eat(plate);
+            }
+        }
+}
